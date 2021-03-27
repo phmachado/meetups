89b8c62809fb069b975e293bffa563d965499464
Pedro Machado <pfnmachado@gmail.com>
2021-03-27 15:01:11 -0300

    passing data with props and dynamic content

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import Todo from "./components/Todo";
+
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo title="My Title 1" />
+      <Todo title="My Title 2" />
+      <Todo title="My Title 3" />
+    </div>
+  );
 }
 
 export default App;
