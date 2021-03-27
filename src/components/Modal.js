dc82214fc3ca1d0474d5a717c10e610d7e78c124
Pedro Machado <pfnmachado@gmail.com>
2021-03-27 15:31:19 -0300

    state and working with event props

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,9 +1,13 @@
-function Modal() {
+function Modal(props) {
   return (
     <div className="modal">
       <p>Are you sure?</p>
-      <button className="btn btn--alt">Cancel</button>
-      <button className="btn">Confirm</button>
+      <button className="btn btn--alt" onClick={props.closeModalHandler}>
+        Cancel
+      </button>
+      <button className="btn" onClick={props.closeModalHandler}>
+        Confirm
+      </button>
     </div>
   );
 }
