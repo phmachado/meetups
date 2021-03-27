55c58bac4a41db5ac966453949f1cb1b7e316c7f
Pedro Machado <pfnmachado@gmail.com>
2021-03-27 16:02:09 -0300

    links and navigation

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { Route, Switch } from "react-router-dom";
 import AllMeetups from "./pages/AllMeetups";
 import Favorites from "./pages/Favorites";
 import NewMeetup from "./pages/NewMeetup";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
   return (
     <div>
+      <MainNavigation />
       <Switch>
         <Route exact path="/">
           <AllMeetups />
