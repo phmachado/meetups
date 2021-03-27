0ffd715794c9610dea2d9177d8636ddb98f08015
Pedro Machado <pfnmachado@gmail.com>
2021-03-27 15:13:41 -0300

    handling events and adding more components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import Modal from "./components/Modal";
 import Todo from "./components/Todo";
+import Backdrop from "./components/Backdrop";
 
 function App() {
   return (
@@ -7,6 +9,8 @@ function App() {
       <Todo title="My Title 1" />
       <Todo title="My Title 2" />
       <Todo title="My Title 3" />
+      <Modal />
+      <Backdrop />
     </div>
   );
 }
