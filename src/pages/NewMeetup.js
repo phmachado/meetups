423786cde0007284d22cdf630fc5b70cc6c67108
Pedro Machado <pfnmachado@gmail.com>
2021-03-27 22:37:58 -0300

    sending a post http request

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,20 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetup() {
+  function addMeetupHandler(meetupData) {
+    fetch("https://db-meetups-default-rtdb.firebaseio.com/meetups.json", {
+      method: "POST",
+      body: JSON.stringify(meetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
   return (
     <section>
-      <h1>New Meetup Page</h1>
-      <NewMeetupForm />
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
